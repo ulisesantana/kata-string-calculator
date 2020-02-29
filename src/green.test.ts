41539d0630c82215a1a30ea5e8253b35c4b44fd3
Ulises Santana <ulisesantana@gmail.com>
2020-02-29 14:48:15 +0000

    :tada: initial commit

--- src/green.test.ts
+++ src/green.test.ts
@@ -1,5 +1,25 @@
-describe('Basic tests', () => {
-	it('Always green', () => {
-		expect(true).toBe(true);
+// "//1/" > throw exception 
+// "///" > throw exception 
+
+import {calculate} from '.';
+
+describe('String calculator should', () => {
+	it('add numbers separated by commas', () => {
+		expect(calculate('1,1,1')).toBe(3);
+	});
+
+	it('ignore characters if they are not valid numbers', () => {
+		expect(calculate('non-numeric')).toBe(0);
+		expect(calculate('irrelevant,1')).toBe(1);
+	});
+
+	it('accept a custom separator given by the expression', () => {
+		expect(calculate('//#/4#3')).toBe(7);
+		expect(calculate('//<>/4<>3')).toBe(7);
+	});
+
+	it('accept a custom separator given by the expression and ignore characters if they are not valid numbers', () => {
+		expect(calculate("//@/1,2@3" )).toBe(15);
+		expect(calculate("//@/1,2lha6SADV7adf@3" )).toBe(1270);
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,60 @@
-console.log('Hello world!');
+// IsCalculable means is a string with numbers and alphanumeric characters
+const isCalculable = (x: string): boolean =>
+	new RegExp(/\d+(A-z)*.\d/, 'g').test(x);
+const isNumber = (x: string): boolean => !isNaN(Number(x)) || isCalculable(x);
+const addReducer = (acc: number, x: string): number => acc + Number(x);
+const addStringReducer = (acc: string, x: string | string[]): string =>
+	acc + String(x);
+const removeAlphanumericCharacters = (x: string): string =>
+	[...x].filter(isNumber).reduce(addStringReducer);
+
+function addNumbersFrom(expression: string, separator = ','): number {
+	return expression
+		.split(separator)
+		.filter(isNumber)
+		.map(removeAlphanumericCharacters)
+		.reduce(addReducer, 0);
+}
+
+function getExpressionSeparatorPositions(
+	expression: string
+): Record<string, number> {
+	const positionOfSeparatorStart = 2;
+	const positionOfConfigurationEnd = expression.indexOf(
+		'/',
+		positionOfSeparatorStart
+	);
+	const positionOfCalculableExpression = positionOfConfigurationEnd + 1;
+
+	return {
+		positionOfSeparatorStart,
+		positionOfConfigurationEnd,
+		positionOfCalculableExpression
+	};
+}
+
+export function calculate(expression: string): number {
+	if (expression.startsWith('//')) {
+		const {
+			positionOfSeparatorStart,
+			positionOfConfigurationEnd,
+			positionOfCalculableExpression
+		} = getExpressionSeparatorPositions(expression);
+
+		if (expression[positionOfConfigurationEnd] === '/') {
+			const separator = expression.slice(
+				positionOfSeparatorStart,
+				positionOfConfigurationEnd
+			);
+
+			return addNumbersFrom(
+				expression.slice(positionOfCalculableExpression),
+				separator
+			);
+		}
+
+		throw new Error('Non expected expression');
+	} else {
+		return addNumbersFrom(expression);
+	}
+}
